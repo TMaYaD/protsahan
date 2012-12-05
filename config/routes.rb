bb808b5fa0c8c22f46cc7ac517f1439af36ff434
Subhash Chandra <TMaYaD@LoonyB.in>
2012-12-05 12:46:23 +0530

    changed the home page controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Protsahan::Application.routes.draw do
-  root :to => 'high_voltage/pages#show', :id => 'home'
+  root :to => 'articles#show', :id => 'home'
   match '/pages/*id' => 'high_voltage/pages#show', :as => :page, :format => false
   match '/*id' => 'articles#show', :as => :article, :format => false
 end
